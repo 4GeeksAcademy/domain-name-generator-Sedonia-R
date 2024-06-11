aaacf105edce7d6b26c544f5c4f215f831ab281f
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-06-11 00:11:33 +0000

    refactored app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
+const pronouns = ["the", "our"];
+const adjectives = ["great", "big"];
+const nouns = ["jogger", "racoon"];
 
 const domains = [];
-for (let index = 0; index < pronoun.length; index++) {
-  for (let secondIndex = 0; secondIndex < adj.length; secondIndex++) {
-    for (let thirdIndex = 0; thirdIndex < noun.length; thirdIndex++) {
-      domains.push(`${pronoun[index]}${adj[secondIndex]}${noun[thirdIndex]}`);
+for (let pronoun in pronouns) {
+  for (let adjective in adjectives) {
+    for (let noun in nouns) {
+      domains.push(`${pronouns[pronoun]}${adjectives[adjective]}${nouns[noun]}`);
     }
   }
 }
